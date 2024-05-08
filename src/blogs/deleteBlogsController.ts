cc60ddebe30a7d90bba9f86e1a85a036943e6a63
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-05-08 10:36:34 +0600

    Added new checks

--- src/blogs/deleteBlogsController.ts
+++ src/blogs/deleteBlogsController.ts
@@ -4,5 +4,8 @@ import { deleteBlogsRepository } from "./deleteBlogsRepository"
 
 export const deleteBlogsController = async (req: Request, res: Response) => {
     const blog = await deleteBlogsRepository.delete(req.params.id)
+    if(!blog) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+    }
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 }
