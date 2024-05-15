97ed0842df438c0de39fa02c0521cb4cbe145700
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-05-15 12:49:14 +0600

    Fixed id blog

--- src/blogs/getBlogsRepository.ts
+++ src/blogs/getBlogsRepository.ts
@@ -3,13 +3,19 @@ import { blogCollection } from "../db/mongo-db"
 export const getBlogsRepository = {
     async getAllBlogs() {
         const blogsArr = await blogCollection.find({}).toArray();
-        blogsArr.map(blog => {
+        const mappedArr = blogsArr.map(blog => {
             const mappedBlog = {
                     id: blog._id,
-                    ...blog
+                    name: blog.name,
+                    description: blog.description,
+                    isMembership: blog.isMembership,
+                    websiteUrl: blog.websiteUrl,
+                    createdAt: blog.createdAt
             }
+            console.log("mapped", mappedBlog)
             return mappedBlog
         })
-        return blogsArr
+        console.log("blogs", blogsArr)
+        return mappedArr
     }
 }
