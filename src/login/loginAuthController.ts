0d22786a7734a24fdfda91c9f44b62fe864d468e
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-08-19 16:15:32 +0400

    Fixed auth error type

--- src/login/loginAuthController.ts
+++ src/login/loginAuthController.ts
@@ -6,8 +6,8 @@ import { HTTP_STATUSES } from "../settings"
 export const loginAuthController = async (req: Request, res: Response) => {
     const userInfo = await loginAuthService.loginUser(req.body)
     if(userInfo.status === StatusCode.Unauthtorized) {
-        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+        res.status(HTTP_STATUSES.UNAUTHORIZED_401).json("Wrong password or login")
         return
     }
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+    res.status(HTTP_STATUSES.NO_CONTENT_204).json("User is logged in")
 }
